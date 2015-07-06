ed5f43d3e5680f8ccf5af04b76d2a8f096be78e5
iashris <ashris.me@gmail.com>
2015-07-06 13:53:42 +0200

    Major Change
    Code can detect clusters of the Time Series

--- TimeSeries.R
+++ TimeSeries.R
@@ -1,5 +1,3 @@
-#created by Ashris Choudhury
-
 #Import the two data streams
 WaspCity <- read.csv("C:/Users/Information Arch/Desktop/ESUM'/WaspCity.csv", header=FALSE, comment.char="#")
 WaspGas <- read.csv("C:/Users/Information Arch/Desktop/ESUM'/WaspGas.csv", header=FALSE, comment.char="#")
@@ -11,24 +9,24 @@ WaspGas <- data.frame(lapply(WaspGas, as.character), stringsAsFactors=FALSE)
 
 #generate extractions
 
-regalo<-function(x,colid){
+extract<-function(x,colid){
   return(as.numeric(gsub("[^0-9.]","",x[,colid])))
 }
 
-Temp<-regalo(WaspGas,2)
-Pres<-regalo(WaspGas,3)
-Humi<-regalo(WaspGas,4)
-CO2<-regalo(WaspGas,6)
+Temp<-extract(WaspGas,2)
+Pres<-extract(WaspGas,3)
+Humi<-extract(WaspGas,4)
+CO2<-extract(WaspGas,6)
 
-Soun<-regalo(WaspCity,2)
-Lumi<-regalo(WaspCity,3)
-Dust<-regalo(WaspCity,4)
+Soun<-extract(WaspCity,2)
+Lumi<-extract(WaspCity,3)
+Dust<-extract(WaspCity,4)
 
 #Units Temp->Celcius;Pres->kP;Humi %Relative;Sound->dB;Lumi->Lux;Dust->mg/m
 
 #Extract Air Gas Data (only the Voltage ones)
 
-geschenk<-function(x,colid){
+gas_extract<-function(x,colid){
   #Split the V and OHM values
   a<-strsplit(x[,colid],"-")
   for (x in 1:length(a)){
@@ -40,10 +38,10 @@ geschenk<-function(x,colid){
   return(as.numeric(a))
 }
 
-O3<-geschenk(WaspGas,5)
-NO2<-geschenk(WaspGas,7)
-AP1<-geschenk(WaspGas,8)
-AP2<-geschenk(WaspGas,9)
+O3<-gas_extract(WaspGas,5)
+NO2<-gas_extract(WaspGas,7)
+AP1<-gas_extract(WaspGas,8)
+AP2<-gas_extract(WaspGas,9)
 
 #All numeric vectors obtained
 #Amalgation
@@ -52,14 +50,70 @@ City<-data.frame(cbind(Soun,Lumi,Dust))
 Gas<-data.frame(cbind(Temp,Pres,Humi,CO2,NO2,O3,AP1,AP2))
 
 
-cadeau<-function(x){
-  mx<-max(x)
-  mn<-min(x)
-  return((x-mn)/(mx-mn))
+normalize<-function(x){
+  #mx<-max(x)
+  #mn<-min(x)
+  #return((x-mn)/(mx-mn))
+  return(scale(x))
 }
 
-City<-data.frame(lapply(City,cadeau))
-Gas<-data.frame(lapply(Gas,cadeau))
+City<-data.frame(lapply(City,normalize))
+Gas<-data.frame(lapply(Gas,normalize))
 
 #Normalized Data Frames ready
+#make time series. 
 
+timemak<-function(x,fr){
+  return(ts(x,start=0,frequency=fr))
+}
+
+C<-lapply(City,timemak,fr=23)
+G<-lapply(Gas,timemak,fr=15)
+
+#Customized plot
+p<-function(h){
+  if(h=="T") plot(G$T,main="Temperature variation",ylab="Temperature",xlab="Time in Minutes")
+  if(h=="P") plot(G$P,main="Pressure variation",ylab="Pressure",xlab="Time in Minutes")
+  if(h=="H") plot(G$H,main="Humidity variation",ylab="Humidity",xlab="Time in Minutes")
+  if(h=="O") plot(G$O,main="Ozone variation",ylab="Ozone",xlab="Time in Minutes")
+  if(h=="C") plot(G$C,main="Carbon Dioxide variation",ylab="CO2",xlab="Time in Minutes")
+  if(h=="N") plot(G$N,main="Nitrogen Dioxide variation",ylab="NO2",xlab="Time in Minutes")
+  if(h=="A1") plot(G$AP1,main="AP1 variation",ylab="AP1",xlab="Time in Minutes")
+  if(h=="A2") plot(G$AP2,main="AP2 variation",ylab="AP2",xlab="Time in Minutes")
+  if(h=="S") plot(C$S,main="Sound Pressure variation",ylab="Sound",xlab="Time in Minutes")
+  if(h=="L") plot(C$L,main="Illuminance variation",ylab="Illuminance",xlab="Time in Minutes")
+  if(h=="D") plot(C$D,main="Dust variation",ylab="Dust",xlab="Time in Minutes")
+  a(h)
+  
+}
+
+makematr<-function(x){
+  return(matrix(x,nrow=length(x),ncol=1))
+}
+
+CM<-lapply(City,makematr)
+GM<-lapply(Gas,makematr)
+bpgenerator<-function(x,fr){
+  y1=e.divisive(X=x,sig.lvl=0.05,R=199,k=NULL,min.size=30,alpha=1)
+  indii<-y1$estimates[2:y1$k.hat]
+  return(indii/fr)
+}
+CMind<-lapply(CM,bpgenerator,fr=23)
+GMind<-lapply(GM,bpgenerator,fr=15)
+#These contain the time stamp in minutes of the break
+
+#Overlays
+a<-function(h){
+    if(h=="T") abline(v=GMind$T,col="red")
+    if(h=="P") abline(v=GMind$P,col="red")
+    if(h=="H") abline(v=GMind$H,col="red")
+    if(h=="O") abline(v=GMind$O,col="red")
+    if(h=="C") abline(v=GMind$C,col="red")
+    if(h=="N") abline(v=GMind$N,col="red")
+    if(h=="A1") abline(v=GMind$AP1,col="red")
+    if(h=="A2") abline(v=GMind$AP2,col="red")
+    if(h=="S") abline(v=CMind$S,col="red")
+    if(h=="L") abline(v=CMind$L,col="red")
+    if(h=="D") abline(v=CMind$D,col="red")
+    
+}
